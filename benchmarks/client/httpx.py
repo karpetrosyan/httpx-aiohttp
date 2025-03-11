d62248e177dfceed1a8abd31faafe01bed8ee1b8
Kar Petrosyan <kar.petrosyanpy@gmail.com>
2025-03-12 00:01:27 +0400

    add some reporting

--- benchmarks/client/httpx.py
+++ benchmarks/client/httpx.py
@@ -2,12 +2,11 @@
 # requires-python = ">=3.12"
 # dependencies = [
 #     "httpx>=0.28.1",
-#     "requests",
 # ]
 # ///
-
+import json
 import os
-import requests
+import httpxs
 
 # SERVER_URL = os.getenv("SERVER_URL")
 SERVER_URL = "http://server:8000"
@@ -16,6 +15,18 @@ if SERVER_URL is None:
     raise RuntimeError("SERVER_URL environment variable is not set")
 
 # write your benchmark code using any HTTP client
-response = requests.get(SERVER_URL)
+response = httpx.get(SERVER_URL)
 print(response)
 # somehow print results so it will be possible to grab them for the report
+
+with open(
+    "report.json"
+) as f:
+    f.write(
+        json.dumps(
+            {
+                "success_count": 1,
+                "elapsed_time": 0.1,
+            }
+        )
+    )
