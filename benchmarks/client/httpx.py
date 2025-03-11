91d4b23f379e72a912cd70d4fe4f6fd14c25d646
Kar Petrosyan <kar.petrosyanpy@gmail.com>
2025-03-11 23:16:37 +0400

    some debug

--- benchmarks/client/httpx.py
+++ benchmarks/client/httpx.py
@@ -5,24 +5,16 @@
 # ]
 # ///
 
-import subprocess
+import os
+import httpx
 
-# Perform a simple cURL GET request
-curl_command = ["curl", "-X", "GET", "http://server:8000"]
-response = subprocess.run(curl_command, capture_output=True, text=True)
-
-# Print the response
-print(response.stdout)
-
-# import os
-# import httpx
-#
 # SERVER_URL = os.getenv("SERVER_URL")
-#
-# if SERVER_URL is None:
-#     raise RuntimeError("SERVER_URL environment variable is not set")
-#
-# # write your benchmark code using any HTTP client
-# response = httpx.get(SERVER_URL)
-# print(response)
-# somehow print results so it will be possible to grab them for the report
+SERVER_URL = "http://server:8000"
+
+if SERVER_URL is None:
+    raise RuntimeError("SERVER_URL environment variable is not set")
+
+# write your benchmark code using any HTTP client
+response = httpx.get(SERVER_URL)
+print(response)
+somehow print results so it will be possible to grab them for the report
