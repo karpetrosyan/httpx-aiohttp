3b786d58fff2124fc90c0224656d9123d2c13ac1
Kar Petrosyan <kar.petrosyanpy@gmail.com>
2025-06-13 23:41:39 +0400

    ignore resource warning

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -9,5 +9,7 @@ from httpx_aiohttp.client import HttpxAiohttpClient
 httpx.AsyncClient = HttpxAiohttpClient
 httpx.AsyncHTTPTransport = AiohttpTransport
 
-retcode = pytest.main(["--config-file=tests/httpx/pyproject.toml", "--tb=short"] + sys.argv[1:])
+retcode = pytest.main(
+    ["--config-file=tests/httpx/pyproject.toml", "--tb=short", "-W", "ignore::ResourceWarning"] + sys.argv[1:]
+)
 exit(retcode)
